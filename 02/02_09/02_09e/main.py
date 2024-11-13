0234f23d0254efe1c652807989971def1d154f55
Alexis Montalvo <alexis.1007@hotmail.com>
2024-11-13 17:38:04 +0000

    task 2.9

--- 02/02_09/02_09e/main.py
+++ 02/02_09/02_09e/main.py
@@ -1,4 +1,11 @@
 def find_second_smallest(my_list):
-    return 0
+    if len(my_list)>1:
+        m=my_list[0]
+        for t in my_list:
+            if t < m :
+                sec=m
+                m=t
+        return sec
+    return None  
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
