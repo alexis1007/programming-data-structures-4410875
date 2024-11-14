9e42f1ba6f0ba22ec8cf2ac7174a0308d249f1a1
Alexis Montalvo <alexis.1007@hotmail.com>
2024-11-14 13:26:29 +0000

    Task 05

---
+++ 05/05_04/05_04b/main.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def Binare(numero):
+  c = deque()
+  fin = False
+  while fin == False:
+    t = 0
+    for n in numero:
+      if t == 0:
+        t = t + 1
+        
+
+
+
+    
+
