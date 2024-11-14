11a5951c9ef1d1b4f0085bd0058ff079541497af
Alexis Montalvo <alexis.1007@hotmail.com>
2024-11-14 16:49:46 +0000

    Task 5 - binary converter

--- 05/05_04/05_04b/main.py
+++ 05/05_04/05_04b/main.py
@@ -1,15 +1,43 @@
 from collections import deque
 
-def Binare(numero):
-  c = deque()
-  fin = False
-  while fin == False:
-    t = 0
-    for n in numero:
-      if t == 0:
-        t = t + 1
-        
+def pop(pila):
+  if len(pila)==0:
+      pila.append(1)
+  else:
+    t = pila.pop()
+    if t == 1:
+      pop(pila)
+      pila.append(0)
+    if t == 0:
+      pila.append(1)
 
+def conver(deque):
+  ret=[None] * len(deque)
+  i = 0
+  while len(deque) > 0:
+    ret[i] = deque.pop()
+    i += 1
+  return ret
+
+def obtener(task2):
+  while len(task2) >0:
+    print(task2.popleft())
+
+def Binare(numero):
+  task = deque()
+  c = deque()
+  tem = deque()
+  n = 0
+  while n < numero:
+    n += 1
+    pop(c)
+    tem = c.copy()
+    task.append(conver(tem))
+  obtener(task)
+
+
+          
+Binare(5)
 
 
     
